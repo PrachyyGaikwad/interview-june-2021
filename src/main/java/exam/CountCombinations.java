4c271a6fc89f548ed393294a146317a983d456b6
PrachyyGaikwad <86194547+PrachyyGaikwad@users.noreply.github.com>
2021-06-22 12:56:22 +0530

    Count Combination code

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -2,9 +2,33 @@ package exam;
 
 public class CountCombinations {
 
-  public int getNumberOfWays(final int total, final int k) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
-  }
+  static int ways(int n)
+{
+    // If there is a single number left it will form singleton 
 
+    if (n == 1) 
+    {
+        return 1;
+    }
+
+    // if there are just 2 numbers left, they will form a pair
+
+    if (n == 2) 
+    {
+        return 2;
+    }
+
+    else
+    {
+        return ways(n-1) + ways(n-2);
+    }
+}
+ 
+// Driver Code
+
+public static void main (String[] args) 
+{
+    int n = 5;
+   System.out.println("Number of ways = "+ ways(n));
+}
 }
